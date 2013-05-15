ff217ef1ea2dd73f358a554e1859a893cf0de07b
Ómar Kjartan Yasin <omarkj@gmail.com>
2013-05-15 01:57:02 +0000

    Preserve jQuery

--- public/grid.js
+++ public/grid.js
@@ -15,16 +15,27 @@
      head.appendChild(script);
    }
    var start = function(callback) {
+     var old_jq = undefined;
      if (__g__.LMD.$) {
        next();
      } else if (__g__.jQuery && __g__.jQuery.fn.jquery == '2.0.0') {
        __g__.LMD.$ = window.jQuery;
        next();
      } else {
+       if (window.jQuery) {
+	 // There is another jQuery here, not of the version we want. Let's
+	 // keep it around
+	 old_jq = window.jQuery.noConflict();
+       }
        // It's not here. We got to download it..
        load_script('//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js',
 	 function() {
 	   __g__.LMD.$ = jQuery.noConflict();
+	   if (old_jq) {
+	     // there is another jQuery around here, give it back
+	     __g__.jQuery = old_jq.noConflict();
+	     __g__.$ = __g__.jQuery;
+	   }
 	   callback();
 	 });
      }
